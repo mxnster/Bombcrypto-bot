9155c6834d08de9eaf14ee98b9605a2d438198c5
Александр Флягин <globallll007@gmail.com>
2022-02-01 02:16:16 +0300

    Some fixes

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    privateKey: '', // private key для импорта кошелька
-    password: '', // пароль, который будет установлен для метмаска
-    telegramAPI: '', // через botFather создать бота и получить apikey
-    CHAT_ID: '', // получить id можно тут: @userinfobot
-    workersInterval: 30,  // интревал отправки работяг на работу в минутах
-    onlyGreenStamina: true, //отправлять в работу только с зеленой стаминой (true/false)
-    refreshMapInterval: 5 // интервал очистки карты
+    privateKey: '', // wallet private key
+    password: '', // random password for wallet creation
+    telegramAPI: '', // get telegram API key from @BotFather
+    CHAT_ID: '', // you can get it here: @userinfobot
+    workersInterval: 30,  // interval for sending workers to work (minutes)
+    onlyGreenStamina: true, // send to work only with green stamina (true/false)
+    refreshMapInterval: 5 // map refresh interval (minutes)
 }
